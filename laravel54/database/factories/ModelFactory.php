97340550c668487e921457fe3538270fe10cad74
chenlei <chenlei163mail@163.com>
2018-02-10 00:03:18 +0800

    使用Faker进行数据填充

--- laravel54/database/factories/ModelFactory.php
+++ laravel54/database/factories/ModelFactory.php
@@ -11,6 +11,12 @@
 |
 */
 
+/*
+➜  laravel54 git:(master) ✗ php artisan tinker
+Psy Shell v0.8.17 (PHP 7.1.12 — cli) by Justin Hileman
+>>> factory(App\Post::class,10)->create();
+ */
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
@@ -22,3 +28,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence(3),
+        'content' => $faker->paragraph(8),
+        'user_id' => $faker->numberBetween($min = 1, $max = 100),
+    ];
+});
+
