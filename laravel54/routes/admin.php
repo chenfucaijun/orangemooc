cb776e7910c729597216d4d77fe62bff1a9943f1
chenfucaijun <chenlei163mail@163.com>
2018-03-03 02:50:43 +0800

    Gate实现权限管理的注册和使用

--- laravel54/routes/admin.php
+++ laravel54/routes/admin.php
@@ -11,39 +11,41 @@ Route::group(['prefix' => 'admin'], function () {
 
 
     //增加中间件
-    Route::group(['middleware'=>'auth:admin'],function (){
+    Route::group(['middleware' => 'auth:admin'], function () {
         //首页
         Route::get('home', '\App\Admin\Controllers\HomeController@index');
 
-        // 用户管理
-        Route::get('/users', '\App\Admin\Controllers\UserController@index');
-        Route::get('/users/create', '\App\Admin\Controllers\UserController@create');
-        Route::post('/users/store', '\App\Admin\Controllers\UserController@store');
-        Route::get('/users/{user}/role', '\App\Admin\Controllers\UserController@role');
-        Route::post('/users/{user}/role', '\App\Admin\Controllers\UserController@storeRole');
+
+        //管理人员权限中间件
+        Route::group(['middleware' => 'can:system'], function () {
+            // 管理人员模块：用户管理
+            Route::get('/users', '\App\Admin\Controllers\UserController@index');
+            Route::get('/users/create', '\App\Admin\Controllers\UserController@create');
+            Route::post('/users/store', '\App\Admin\Controllers\UserController@store');
+            Route::get('/users/{user}/role', '\App\Admin\Controllers\UserController@role');
+            Route::post('/users/{user}/role', '\App\Admin\Controllers\UserController@storeRole');
+            // 管理人员模块：角色管理
+            Route::get('/roles', '\App\Admin\Controllers\RoleController@index');
+            Route::get('/roles/create', '\App\Admin\Controllers\RoleController@create');
+            Route::post('/roles/store', '\App\Admin\Controllers\RoleController@store');
+            Route::get('/roles/{role}/permission', '\App\Admin\Controllers\RoleController@permission');
+            Route::post('/roles/{role}/permission', '\App\Admin\Controllers\RoleController@storePermission');
+            // 管理人员模块：权限管理
+            Route::get('/permissions', '\App\Admin\Controllers\PermissionController@index');
+            Route::get('/permissions/create', '\App\Admin\Controllers\PermissionController@create');
+            Route::post('/permissions/store', '\App\Admin\Controllers\PermissionController@store');
+        });
+
+        Route::group(['middleware' => 'can:post'], function (){
+            //文章审核模块
+            Route::get('/posts', '\App\Admin\Controllers\PostController@index');
+            Route::post('/posts/{post}/status', '\App\Admin\Controllers\PostController@status');
+        });
 
 
-        //审核模块
-        Route::get('/posts','\App\Admin\Controllers\PostController@index');
-        Route::post('/posts/{post}/status','\App\Admin\Controllers\PostController@status');
 
-
-        // 角色管理
-        Route::get('/roles', '\App\Admin\Controllers\RoleController@index');
-        Route::get('/roles/create', '\App\Admin\Controllers\RoleController@create');
-        Route::post('/roles/store', '\App\Admin\Controllers\RoleController@store');
-        Route::get('/roles/{role}/permission', '\App\Admin\Controllers\RoleController@permission');
-        Route::post('/roles/{role}/permission', '\App\Admin\Controllers\RoleController@storePermission');
-
-
-        // 权限管理
-        Route::get('/permissions', '\App\Admin\Controllers\PermissionController@index');
-        Route::get('/permissions/create', '\App\Admin\Controllers\PermissionController@create');
-        Route::post('/permissions/store', '\App\Admin\Controllers\PermissionController@store');
 
     });
-
-
 
 
 });
