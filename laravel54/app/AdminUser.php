586d91c23256d50be267f5371647437a74a68e3d
chenfucaijun <chenlei163mail@163.com>
2018-03-03 01:50:07 +0800

    角色&权限&用户的模型关联

--- laravel54/app/AdminUser.php
+++ laravel54/app/AdminUser.php
@@ -14,4 +14,49 @@ class AdminUser extends Authenticatable
     protected $guarded = [];
 
 
+    /*
+     * 用户有哪些角色
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(\App\AdminRole::class, 'admin_role_user', 'user_id', 'role_id')->withPivot(['user_id', 'role_id']);
+    }
+
+    /*
+     * 是否有某个角色
+     */
+    public function isInRoles($roles)
+    {
+        //是否有交集
+        //两个感叹号返回布尔值
+        return !!$roles->intersect($this->roles)->count();
+    }
+
+    /*
+    * 给用户分配角色
+    */
+    public function assignRole($roleName)
+    {
+        $role = \App\AdminRole::where('name', $roleName)->first();
+        return $this->roles()->save($role);
+    }
+
+    /*
+     * 删除user和role的关联
+     */
+    public function deleteRole($role)
+    {
+        return $this->roles()->detach($role);
+    }
+
+
+    /*
+     * 是否有权限
+     */
+    public function hasPermission($permission)
+    {
+        return $this->isInRoles($permission->roles);
+    }
+
+
 }
